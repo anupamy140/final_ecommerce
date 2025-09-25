c4d190d526fb6d3e1d50064a2f1ad7f6dd50ed9c
premOFbounteous <prem.maity@bounteous.com>
2025-09-25 15:59:06 +0530

    rectified img url for category in deployment

--- src/components/shared/CategoryCarousel.tsx
+++ src/components/shared/CategoryCarousel.tsx
@@ -24,7 +24,7 @@ export const CategoryCarousel = ({ categories, onCategorySelect, selectedCategor
                     {categories.map((category) => (
                         <div key={category} onClick={() => onCategorySelect(category)} className={`flex-shrink-0 w-60 cursor-pointer transition-all duration-500 ease-in-out ${selectedCategory && selectedCategory !== category ? 'opacity-50 scale-95' : 'opacity-100 scale-100'} group-hover:opacity-50 group-hover:scale-95 hover:!opacity-100 hover:!scale-105 hover:-translate-y-4`}>
                             <div className={`relative h-[28rem] w-full bg-gray-200 overflow-hidden transition-all duration-300 rounded-2xl ${selectedCategory === category ? 'ring-4 ring-gray-900 dark:ring-yellow-400 ring-offset-4 ring-offset-gray-50 dark:ring-offset-gray-950' : ''}`}>
-                                <ImageWithLoader src={`/public/${category}.jpg`} alt={category} className="w-full h-full" />
+                                <ImageWithLoader src={`${category}.jpg`} alt={category} className="w-full h-full" />
                                 <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
                                 <h3 className="absolute bottom-5 left-5 text-2xl font-bold text-white capitalize">{category.replace(/-/g, ' ')}</h3>
                             </div>
