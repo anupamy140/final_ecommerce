f2f4b956b00edf81deab1ffadf932c27f9fded32
anupamy140 <Anupam.Yadav@bounteous.com>
2025-09-26 11:57:17 +0530

    Manual Chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          // Create a separate chunk for the three.js library
+          if (id.includes('three')) {
+            return 'three';
+          }
+          // Create a separate chunk for the gsap library
+          if (id.includes('gsap')) {
+            return 'gsap';
+          }
+        },
+      },
+    },
+  },
 })
